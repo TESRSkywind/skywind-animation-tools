2a1f835a69c1b523e7ee3976f93c200d831bf8cc
Cole <obriencole11@gmail.com>
2025-09-02 00:05:30 -0700

    Added tag support for Maya import

--- skywind/maya/commands/open_animation.py
+++ skywind/maya/commands/open_animation.py
@@ -77,6 +77,30 @@ def _import_fbx(filepath: str, update=True) -> list[str]:
     return list(nodes)
 
 
+def _copy_tag_attributes(source_root: str, destination_root: str):
+    for srcAttrName in cmds.listAttr(source_root, userDefined=True):
+        if not cmds.attributeQuery(srcAttrName, node=destination_root, exists=True):
+            sel = om2.MSelectionList()
+            sel.add('%s.%s' % (source_root, srcAttrName))
+            cmd = om2.MFnAttribute(sel.getPlug(0).attribute()).getAddAttrCmd(True)
+            cmd = cmd.replace(';', ' %s;' % destination_root)
+            mel.eval(cmd)
+
+        cmds.copyAttr(source_root, destination_root, at=[srcAttrName], v=True)
+
+        # Copy Animation
+        if cmds.keyframe(source_root, at=[srcAttrName], q=True):
+            cmds.copyKey(source_root, at=[srcAttrName])
+            cmds.pasteKey(destination_root, at=[srcAttrName])
+
+
+def _get_root_joint(nodes: list[str]) -> str | None:
+    for node in cmds.ls(nodes, type='joint'):
+        if cmds.listRelatives(node, parent=True) is None:
+            return node
+    return None
+
+
 def open_animation(fbx_file: str | None = None, reference: bool = True):
 
     fbx_file = fbx_file or _open_file_dialog()
@@ -89,15 +113,19 @@ def open_animation(fbx_file: str | None = None, reference: bool = True):
 
     # Import control rig
     rig_namespace = os.path.basename(actor.maya_rig).split('.')[0]
-    rig_nodes = cmds.file(
-        actor.maya_rig, i=True, type="mayaAscii", ignoreVersion=True, mergeNamespacesOnClash=False,
-        options="v=0;", pr=True, returnNewNodes=True, namespace=rig_namespace
-    )
+    if reference:
+        rig_nodes = cmds.file(
+            actor.maya_rig, type="mayaAscii", returnNewNodes=True, namespace=rig_namespace, reference=True
+        )
+    else:
+        rig_nodes = cmds.file(
+            actor.maya_rig, type="mayaAscii", returnNewNodes=True, namespace=rig_namespace, i=True
+        )
     rig_control_nodes = [node for node in rig_nodes if node.split('|')[-1].split(':')[-1] in actor.maya_import_mapping.keys()]
 
     # Import skeleton
     skeleton_nodes = _import_fbx(actor.skeleton_fbx, update=False)
-    skeleton_nodes = [node for node in skeleton_nodes if node.split('|')[-1] in actor.maya_import_mapping.values()]
+    skeleton_root = _get_root_joint(skeleton_nodes)
 
     # Bind controls to the skeleton
     for control, joint in actor.maya_import_mapping.items():
@@ -110,7 +138,21 @@ def open_animation(fbx_file: str | None = None, reference: bool = True):
         cmds.parentConstraint(joint, f'{rig_namespace}:{control}', maintainOffset=True)
 
     # Import the animation onto the skeleton
-    _import_fbx(fbx_file, update=True)
+    animation_nodes = _import_fbx(fbx_file, update=False)
+    animation_root = _get_root_joint(animation_nodes)
+
+    # Constraint the skeleton to the animation skeleton
+    cmds.parentConstraint(animation_root, skeleton_root)
+    for node in cmds.ls(animation_nodes, type='joint'):
+        for other_node in cmds.ls(skeleton_nodes, type='joint'):
+            if node.split('|')[-1] == other_node.split('|')[-1]:
+                cmds.parentConstraint(node, other_node)
+
+    # Copy tags from the animation
+    for control in rig_control_nodes:
+        if control.split('|')[-1] == animation_root:
+            _copy_tag_attributes(animation_root, control)
+            break
 
     # Bake animation on controls
     try:
@@ -121,4 +163,9 @@ def open_animation(fbx_file: str | None = None, reference: bool = True):
     finally:
         cmds.refresh(su=False)
 
-    cmds.delete(skeleton_nodes)
+    # Remove imported nodes
+    cmds.delete(skeleton_nodes + animation_nodes)
+
+    # Save the file
+    cmds.file(rename=fbx_file.replace('.fbx', '.ma'))
+    cmds.file(save=True, type="mayaAscii")
